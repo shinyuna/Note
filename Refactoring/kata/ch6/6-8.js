57cd91e3a7b04c60a53ea4de3e2732de194f37a7
shinyuna <yunadev01@gmail.com>
2022-09-14 22:12:55 +0900

    Feat: 함수 매개변수 객체화

--- Refactoring/kata/ch6/6-8.js
+++ Refactoring/kata/ch6/6-8.js
@@ -1,5 +1,27 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+export function readingsOutsideRange(readings, range) {
+  return readings.filter((r) => !range.contains(r.temp));
+}
+
+export class NumberRange {
+  #min;
+  #max;
+  constructor(min, max) {
+    this.#min = min
+    this.#max = max
+  }
+
+  get min() {
+    return this.#min
+  }
+
+  get max() {
+    return this.#max
+  }
+
+  contains(number) {
+    return number >= this.#min && number <= this.#max
+  }
+
 }
 
 const station = {
@@ -12,13 +34,13 @@ const station = {
     { temp: 51, time: '2016-11-10 09:50' },
   ],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
+const operationPlan = new NumberRange(51, 53)
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
+const result = readingsOutsideRange(
+  station.readings,
+  operationPlan
 );
+console.log(result)
+
+// 함수의 매개변수를 객체로 전달
+// 데이터 객체를 클래스로 구현하고, 클래스 내부에서 데이터를 처리하는 로직과 함께 두고 관리
