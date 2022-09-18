c672c4388e22a41d20cdb9a5ccf34ae3217ae9ba
shinyuna <yunadev01@gmail.com>
2022-09-18 17:17:31 +0900

    Feat. 임시변수 질의 함수로 바꾸기

--- Refactoring/kata/ch7/7-4.js
+++ Refactoring/kata/ch7/7-4.js
@@ -1,3 +1,12 @@
+/**
+ * [문제]
+ * - 짧은 코드지만 한 눈에 어떤 행동을 하고 있는지 파악이 안된다.
+ * - 상수를 변경하려고 한다.
+ *
+ * [해결]
+ * - 메서드 내부에서 사용되는 임시 변수를 질의 함수로 바꿔 클래스 내부에서나 외부에서의 활용성을 높였다.
+ */
+
 class Order {
   #quantity;
   #item;
@@ -6,10 +15,15 @@ class Order {
     this.#item = item;
   }
 
+  get basePrice() {
+    return this.#quantity * this.#item.price
+  }
+
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.95 : 0.98
+  }
+
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor
   }
 }
