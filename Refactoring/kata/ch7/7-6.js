dfad1e10ebe5172266e6c221fdb8437048132a52
shinyuna <yunadev01@gmail.com>
2022-09-18 17:45:17 +0900

    Feat. 클래스 인라인하기

--- Refactoring/kata/ch7/7-6.js
+++ Refactoring/kata/ch7/7-6.js
@@ -1,4 +1,11 @@
-export class TrackingInformation {
+/**
+ * 밀접된 정보끼린 하나의 클래스로 묶어두는 것이 좋다.
+ *
+ * [문제]
+ * - 불필요하게 클래스로 나누는 것은 오히려 코드 복잡성을 높인다. (가독성 저하)
+ */
+
+export class Shipment {
   #shippingCompany;
   #trackingNumber;
   constructor(trackingNumber, shippingCompany) {
@@ -22,32 +29,13 @@ export class TrackingInformation {
     this.#trackingNumber = arg;
   }
 
-  get display() {
+  get trackingInformation() {
     return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 }
 
-export class Shipment {
-  #trackingInformation;
-  constructor(trackingInformation) {
-    this.#trackingInformation = trackingInformation;
-  }
 
-  get trackingInfo() {
-    return this.#trackingInformation.display;
-  }
+const shipment = new Shipment(999, 'Maersk')
 
-  get trackingInformation() {
-    return this.#trackingInformation;
-  }
-
-  set trackingInformation(trackingInformation) {
-    this.#trackingInformation = trackingInformation;
-  }
-}
-
-const shipment = new Shipment(new TrackingInformation(999, 'Maersk'));
-console.log(shipment.trackingInfo);
-
-shipment.trackingInformation.shippingCompany = 'COSCO';
-console.log(shipment.trackingInfo);
+shipment.shippingCompany = 'COSCO';
+console.log(shipment.trackingInformation);
