cb71c3698afeac0286e59fbd92d6f89958d332f7
shinyuna <yunadev01@gmail.com>
2022-09-18 15:30:07 +0900

    Feat. 레코드 캡슐화 하기
    - 받은 데이터를 set 할 경우 기존 데이터가 변경되지 않도록 주의 한다.
    - object의 경우 얕은 복사를 주의하며, lodash의 cloneDeep 사용

--- Refactoring/kata/ch7/7-1.js
+++ Refactoring/kata/ch7/7-1.js
@@ -1,4 +1,34 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+/**
+ * 해결 방법: 레코드를 클래스로 만들어 캡슐화 한다.
+ *
+ * [중요 포인트]
+ * 프로젝트 내부적에서 필요한 모델은 객체로 데이터를 전달하는 것 보단, 인자를 각각 전달하는 것이 좋다.
+ * --> 백엔드에서 받은 JSON 데이터 또는 외부 라이브러리의 데이터를 한번 캡슐화하는 경우
+ * 어떤 데이터가 읽기 전용인지 아닌지 청사진을 남겨두는 것이 다른 개발자들과의 협업을 할 때도 중요하다.
+ */
+
+class Organization {
+  #name;
+  #country;
+  constructor(name, country) {
+    this.#name = name
+    this.#country = country
+  }
+
+  get name() {
+    return this.#name
+  }
+
+  get country() {
+    return this.#country;
+  }
+
+  set name(name) {
+    this.#name = name
+  }
+}
+
+const organization = new Organization('Acme Gooseberries', 'GB')
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
