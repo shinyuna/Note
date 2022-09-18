6b83dd642d6453ee8d59b3a1f098c1b2299ff943
shinyuna <yunadev01@gmail.com>
2022-09-18 22:09:54 +0900

    Feat. 필드 옮기기
    - 연관 있는 데이터를 같이 둔다.

--- Refactoring/kata/ch8/8-2-1.js
+++ Refactoring/kata/ch8/8-2-1.js
@@ -1,34 +1,47 @@
+/**
+ * [Note]
+ * 함수/필드 옮기기의 목적은 모두 응집도를 높이기 위함이다.
+ * - "최대한 근접한 곳으로 옮긴다." --> 한번에 완벽한 위치에 옮길 순 없음을 항상 인지하자.
+ *
+ * [해결]
+ * 클래스의 역할과 밀접한 데이터를 함께 둔다.
+ * --> discountRate 은 Customer에 있는 것 보단, contract에 위치하는게 좋다.
+ * --> 손님이 할인되는게 아니라 계약서에 할인율이 적혀 있는게 좋겠쥬..?
+ */
+
 export class Customer {
   #name;
-  #discountRate;
   #contract;
   constructor(name, discountRate) {
     this.#name = name;
-    this.#discountRate = discountRate;
-    this.#contract = new CustomerContract(this.dateToday());
-  }
-
-  get discountRate() {
-    return this.#discountRate;
-  }
-
-  becomePreferred() {
-    this.#discountRate += 0.03;
-    // 다른 코드들이 있음...
-  }
-
-  applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this.#discountRate));
+    this.#contract = new CustomerContract(this.dateToday(), discountRate);
   }
 
   dateToday() {
     return new Date();
   }
+
+  applyDiscount(amount) {
+    return amount.subtract(amount.multiply(this.#contract.discountRate));
+  }
+
+  becomePreferred() {
+    let result = this.#contract.discountRate
+    result += 0.03
+    // 다른 코드들이 있음...
+    return result
+  }
 }
 
 class CustomerContract {
   #startDate;
-  constructor(startDate) {
+  #discountRate;
+  constructor(startDate, discountRate) {
     this.#startDate = startDate;
+    this.#discountRate = discountRate;
+  }
+
+  get discountRate() {
+    return this.#discountRate;
   }
 }
--- Refactoring/kata/ch8/8-2-2.js
+++ Refactoring/kata/ch8/8-2-2.js
@@ -1,17 +1,25 @@
+/**
+ * 외부에서 accountType 이 있는지 없는지 모르게 한다.
+ */
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
+
+  }
+
+  get interestRate() {
+    return this._type.interestRate
+  }
+}
+
+class AccountType {
+  constructor(nameString, interestRate) {
+    this._name = nameString;
     this._interestRate = interestRate;
   }
 
   get interestRate() {
     return this._interestRate;
-  }
-}
-
-class AccountType {
-  constructor(nameString) {
-    this._name = nameString;
   }
 }
