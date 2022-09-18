38218f5a12477b6193494a3e81e550b58a0b6c1f
shinyuna <yunadev01@gmail.com>
2022-09-18 21:54:19 +0900

    Feat. 함수 옮기기
    - 굳이 응집되어 있지 않은 함수를 외부로 추출하고
    - 데이터와 연관있는 함수를 응집화 한다.

--- Refactoring/kata/ch8/8-1-1.js
+++ Refactoring/kata/ch8/8-1-1.js
@@ -1,42 +1,50 @@
+/**
+ * [문제]
+ * 함수 자체는 잘 작성했지만, **함수의 위치에서 냄새**가 난다.
+ *
+ * [Note]
+ * - 중첩 함수에서의 캡슐화
+ * --> 행동이 속성을 필요로 하고 있는지, 밀접하게 연관되어 있는지를 파악하며 캡슐화 한다.
+ *
+ * 코드의 응집도를 높일 땐, 서로 밀접하게 연관된 코드인지 확인하는 것이 중요하다.
+ * --> 불필요하게 응집되어 있는 코드는 오히려 코드의 **재사용성을 떨어트리고** **중복 코드**를 낳게 된다.
+ */
+
+function radians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+function distance(p1, p2) {
+  // 포뮬라: http://www.movable-type.co.uk/scripts/latlong.html
+  const EARTH_RADIUS = 3959; // in miles
+  const dLat = radians(p2.lat) - radians(p1.lat);
+  const dLon = radians(p2.lon) - radians(p1.lon);
+  const a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.cos(radians(p2.lat)) *
+    Math.cos(radians(p1.lat)) *
+    Math.pow(Math.sin(dLon / 2), 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return EARTH_RADIUS * c;
+}
+
+function calculateDistance(points) {
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+}
+
 export function trackSummary(points) {
-  const totalTime = calculateTime();
-  const totalDistance = calculateDistance();
-  const pace = totalTime / 60 / totalDistance;
+  const time = 10000;
+  const distance = calculateDistance(points);
+  const pace = time / 60 / distance;
   return {
-    time: totalTime,
-    distance: totalDistance,
-    pace: pace,
+    time,
+    distance,
+    pace,
   };
-
-  function calculateDistance() {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-  }
-
-  function distance(p1, p2) {
-    // 포뮬라: http://www.movable-type.co.uk/scripts/latlong.html
-    const EARTH_RADIUS = 3959; // in miles
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a =
-      Math.pow(Math.sin(dLat / 2), 2) +
-      Math.cos(radians(p2.lat)) *
-        Math.cos(radians(p1.lat)) *
-        Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return EARTH_RADIUS * c;
-  }
-
-  function radians(degrees) {
-    return (degrees * Math.PI) / 180;
-  }
-
-  function calculateTime() {
-    return 10000;
-  }
 }
 
 const newYork = {
--- Refactoring/kata/ch8/8-1-2.js
+++ Refactoring/kata/ch8/8-1-2.js
@@ -1,27 +1,12 @@
-export class Account {
-  constructor(accountType, daysOverdrawn) {
-    this.type = accountType;
-    this._daysOverdrawn = daysOverdrawn;
-  }
-
-  get bankCharge() {
-    let result = 4.5;
-    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
-    return result;
-  }
-
-  get overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this._daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (this._daysOverdrawn - 7) * 0.85;
-    } else return this._daysOverdrawn * 1.75;
-  }
-
-  get daysOverdrawn() {
-    return this._daysOverdrawn;
-  }
-}
+/**
+ * [문제]
+ * 1. 책에선 AccountType에 따라 daysOverdrawn 값이 변하기 때문에 AccountType 클래스에 해당 함수가 있는게 적합하다고 함
+ * - But, 난 현재 코드만 봤을 땐 AccountType은 딱 AccountType에 관련된 것만 갖고 있는게 맞을 것 같고, 계좌의 값을 다루고 있으니 Account 클래스에 있는게 좋을 것 같음.
+ *
+ * [수정한 부분]
+ * - 한 줄로 리턴해주는 코드는 코드가 짧다는 장점이 있지만, 코드가 헷갈리다는 단점이 있어서 코드가 길어지더라도 다음 줄에 리턴 문을 작성해주는 것이 더 깔끔하다.
+ * - private 값에 접근 할 수 있도록 daysOverdrawn getter 함수를 만들어두고 사용하고 있지 않아서 getter 를 사용하도록 수정
+ */
 
 export class AccountType {
   constructor(type) {
@@ -31,3 +16,33 @@ export class AccountType {
     return this._type === 'Premium';
   }
 }
+
+export class Account {
+  constructor(accountType, daysOverdrawn) {
+    this.type = accountType;
+    this._daysOverdrawn = daysOverdrawn; // 일일 한도?
+  }
+
+  get daysOverdrawn() {
+    return this._daysOverdrawn;
+  }
+
+  get bankCharge() {
+    let result = 4.5;
+    if (this.daysOverdrawn > 0) {
+      result += this.overdraftCharge;
+    }
+    return result;
+  }
+
+  get overdraftCharge() {
+    if (this.type.isPremium) {
+      const baseCharge = 10;
+      return this.daysOverdrawn <= 7 ? baseCharge : baseCharge + (this.daysOverdrawn - 7) * 0.85;
+    }
+    return this.daysOverdrawn * 1.75;
+  }
+}
+
+const account = new Account(new AccountType('Premium'), 12)
+console.log(account.overdraftCharge)
