47b8fabebed7bdc09752333de0da0bf1f09c77fb
shinyuna <yunadev01@gmail.com>
2022-09-18 17:33:38 +0900

    Feat. 클래스 추출하기

--- Refactoring/kata/ch7/7-5.js
+++ Refactoring/kata/ch7/7-5.js
@@ -1,11 +1,19 @@
+/**
+ * 클래스 하나당 하나의 책임, 도메인을 갖고 있는 것이 중요하다.
+ *
+ * [문제]
+ * Person 객체에서 전화번호와 관련된 로직이 있다. 이거슨 사람인가 전화번호인가?
+ *
+ * [해결]
+ * - 전화번호 관련된 로직과 데이터를 TelephoneNumber 클래스로 분리한다.
+ */
+
 class Person {
   #name;
-  #officeAreaCode;
-  #officeNumber;
-  constructor(name, areaCode, number) {
+  #telephoneNumber
+  constructor(name, telephoneNumber) {
     this.#name = name;
-    this.#officeAreaCode = areaCode;
-    this.#officeNumber = number;
+    this.#telephoneNumber = telephoneNumber
   }
 
   get name() {
@@ -17,28 +25,42 @@ class Person {
   }
 
   get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
-  }
-
-  get officeAreaCode() {
-    return this.#officeAreaCode;
-  }
-
-  set officeAreaCode(arg) {
-    this.#officeAreaCode = arg;
-  }
-
-  get officeNumber() {
-    return this.#officeNumber;
-  }
-
-  set officeNumber(arg) {
-    this.#officeNumber = arg;
+    return this.#telephoneNumber.toString
   }
 }
 
-const person = new Person('엘리', '010', '12345678');
+class TelephoneNumber {
+  #areaCode
+  #phoneNumber
+  constructor(areaCode, phoneNumber) {
+    this.#areaCode = areaCode
+    this.#phoneNumber = phoneNumber
+  }
+
+  get areaCode() {
+    return this.#areaCode
+  }
+
+  get phoneNumber() {
+    return this.#phoneNumber
+  }
+
+  set areaCode(value) {
+    this.#areaCode = value
+  }
+
+  set phoneNumber(value) {
+    this.#phoneNumber = value
+  }
+
+  get toString() {
+    return `${this.#areaCode}${this.#phoneNumber}`
+  }
+}
+
+const telephone = new TelephoneNumber('010', '12345678')
+const person = new Person('엘리', telephone);
 console.log(person.name);
-console.log(person.officeAreaCode);
-console.log(person.officeNumber);
 console.log(person.telephoneNumber);
+console.log(telephone.areaCode)
+console.log(telephone.phoneNumber)
