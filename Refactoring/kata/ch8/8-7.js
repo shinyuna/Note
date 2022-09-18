907980759b2f40a4ad0a5f17ec6196588dad36c1
shinyuna <yunadev01@gmail.com>
2022-09-19 00:07:26 +0900

    Feat. 반복문 쪼개기

--- Refactoring/kata/ch8/8-7.js
+++ Refactoring/kata/ch8/8-7.js
@@ -1,10 +1,16 @@
+/**
+ * [Note]
+ * 한 개의 반복문에서 한 가지의 일만 하도록 한다.
+ * 코드의 목적을 파악하고 간결하게 코드를 작성할 수 있도록 한다.
+ */
 export function reportYoungestAgeAndTotalSalary(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  let totalSalary = 0;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-    totalSalary += p.salary;
+  return `youngestAge: ${youngestAge(people)}, totalSalary: ${totalSalary(people)}`;
+
+  function youngestAge() {
+    return Math.min(...people.map(p => p.age))
   }
 
-  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+  function totalSalary() {
+    return people.result((total, p) => (total += p.salary), 0)
+  }
 }
