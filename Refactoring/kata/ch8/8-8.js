bcfd93538d645e32c3dcbdf717a94940ce06350c
shinyuna <yunadev01@gmail.com>
2022-09-19 00:40:38 +0900

    Feat. 반복문을 파이프라인으로 바꾸기

--- Refactoring/kata/ch8/8-8.js
+++ Refactoring/kata/ch8/8-8.js
@@ -1,19 +1,14 @@
+/**
+ * 함수형 프로그래밍을 통해 직관적, 단언적, 간편하게 프로그래밍을 할 수 있다.
+ */
 export function acquireData(input) {
-  const lines = input.split('\n');
-  let firstLine = true;
-  const result = [];
-  for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-      continue;
-    }
-    if (line.trim() === '') continue;
-    const record = line.split(',');
-    if (record[1].trim() === 'India') {
-      result.push({ city: record[0].trim(), phone: record[2].trim() });
-    }
-  }
-  return result;
+  return input
+    .split('\n')
+    .slice(1)
+    .filter(line => !!line.trim())
+    .map(line => line.split(','))
+    .filter(record => record[1].trim() === 'India')
+    .map(line => ({ city: line[0].trim(), phone: line[2].trim() }))
 }
 
 const input = `office, country, telephone\n
@@ -23,4 +18,5 @@ Bangalore, India, +91 80 4064 9570\n
 Porto Alegre, Brazil, +55 51 3079 3550\n
 Chennai, India, +91 44 660 44766`;
 const result = acquireData(input);
-console.log(result);
+
+console.log(result)
