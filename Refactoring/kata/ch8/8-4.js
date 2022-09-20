db15bf1f6eb767f1be0d6cdfba0708bc2cbfd2d4
shinyuna <yunadev01@gmail.com>
2022-09-21 00:41:30 +0900

    Feat. 문장을 호출한 곳으로 옮기기
    - 리팩터링 책에서 나온 예시를 확인 후 이해

--- Refactoring/kata/ch8/8-4.js
+++ Refactoring/kata/ch8/8-4.js
@@ -6,6 +6,10 @@
  *
  * [오잉]
  * - 시간이 지남에 따라 location을 사용하는 방법이 달라졌다고 하셨는데, 뭐가 달라졌는지 모르겠음..
+ * [오잉 해결]
+ * - 책에 나온 예제를 살펴보니 listRecentPhotos 함수에서 emitPhotoData 함수 내부의 location 만 상황에 따라 다르게 출력을 하는 문제가 있었다.
+ * --> 간단한 상황에선 변경되야하는 코드만 꺼내 호출하는 곳에 인라인하면 된다.
+ * --> 복잡한 상황에서는 1. 유지할 코드를 따로 다른 함수로 빼두고 2. 상황에 따라 다르게 사용되는 코드를 호출하는 곳에 인라인 한다. 3. 코드가 잘 작동하는지 확인 후 뺴둔 함수를 원래 함수명으로 수정한다.
  *
  * [해결]
  * 1. 문장을 호출한 곳으로 옮기기가... 데이터를 매개변수로 받음으로써 주석을 제거할 수 있는건가..?
@@ -17,6 +21,7 @@
 function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   outStream.write('');
+  outStream.write(`<p>location: ${photo.location}</p>\n`);
   emitPhotoData(outStream, person.photo);
 }
 
@@ -26,6 +31,7 @@ function listRecentPhotos(outStream, photos) {
     .forEach((p) => {
       outStream.write('<div>\n');
       emitPhotoData(outStream, p);
+      outStream.write(`<p>location: ${photo.location}</p>\n`);
       outStream.write('</div>\n');
     });
 }
@@ -33,7 +39,6 @@ function listRecentPhotos(outStream, photos) {
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function recentDateCutoff(day) {
