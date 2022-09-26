4e3f3a264b89c35103d22559d9c80b0cc9a1a96e
신유나 <yunadev01@gmail.com>
2022-09-27 01:17:03 +0900

    Feat: 매직 리터럴 바꾸기

--- Refactoring/kata/ch9/9-6.js
+++ Refactoring/kata/ch9/9-6.js
@@ -1,3 +1,10 @@
+/**
+ * :NOTE
+ * - **매직 넘버/리터럴**: 프로그래밍에서 비즈니스적 의미를 가진 숫자나 문자를 그대로 표현한 것을 칭한다.
+ * - 코드에서 특정한 숫자를 그대로 입력하는 것 보단, 이 숫자의 의미에 해당하는 이름을 상수로 지정해주는게 좋다.
+ */
+
+const STANDARD_GRAVITY = 9.81;
 function potentialEnergy(mass, height) {
-  return mass * 9.81 * height;
+  return mass * STANDARD_GRAVITY * height;
 }
