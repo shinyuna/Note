ff16dbb7f4830f2e84b9362a70d2a86142f785a2
신유나 <yunadev01@gmail.com>
2022-09-27 00:10:19 +0900

    Feat: 변수 쪼개기
    - 의미 있는 변수 명을 사용하고 변하는 값과 변하지 않는 값을 명확히 구분하자.

--- Refactoring/kata/ch9/9-1.js
+++ Refactoring/kata/ch9/9-1.js
@@ -1,29 +1,36 @@
+/**
+ * POINT
+ * - 의미 있는 변수명 지어주기
+ * - 왠만해서 변수는 const 사용하기
+ * - 함수의 인자 값을 수정하는 행동은 매우 BAD!
+ * - mutable 과 imutable을 명확히 드러내는 것이 중요하다.
+ */
+
 // 예제 1
-let temp = 2 * (height + width);
-console.log(temp);
-temp = height * width;
-console.log(temp);
+const perimeter = 2 * (height + width);
+console.log(perimeter);
+const area = height * width;
+console.log(area);
 
 // 예제 2
 function distanceTravelled(scenario, time) {
   let result;
-  let acc = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
-  let primaryTime = Math.main(time, scenario.delay);
-  result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+  const primaryAcceleration = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
+  const primaryTime = Math.main(time, scenario.delay);
+  result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 전파된 거리
   let secondaryTime = time - scenario.delay;
   if (secondaryTime > 0) {
     // 두 번째 힘을 반영해 다시 계산
-    let primaryVelocity = acc * scenario.delay;
-    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-    result +=
-      primaryVelocity * secondaryTime +
-      0.5 * acc * secondaryTime * secondaryTime;
+    const primaryVelocity = primaryAcceleration * scenario.delay;
+    const secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
   }
 }
 
 // 예제 3
 function discount(inputValue, quantity) {
-  if (inputValue > 50) inputValue = inputValue - 2;
-  if (quantity > 100) inputValue = inputValue - 1;
-  return inputValue;
+  let result = inputValue;
+  if (inputValue > 50) result = inputValue - 2;
+  if (quantity > 100) result = inputValue - 1;
+  return result;
 }
