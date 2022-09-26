0fe0c64aaf54c61d36d5cb3d66e45d4a12ae9484
신유나 <yunadev01@gmail.com>
2022-09-27 00:59:48 +0900

    Feat: 참조를 값으로 바꾸기
    - 변경에 취약한 참조는 값이 변경될 때마다 새로운 값으로 만들어 버린다.

--- Refactoring/kata/ch9/9-4.js
+++ Refactoring/kata/ch9/9-4.js
@@ -1,3 +1,16 @@
+/**
+ * :NOTE
+ * - 값: 불변성, 원시 값은 재할당은 가능하지만 변할 순 없다.
+ * - 참조: 가변성, (object) 데이터 자체를 변경 가능하다.
+ *
+ * [참조 값의 문제]
+ * - 실수로 참조 값 자체를 리턴하게 되면, 외부에서 내부 메서드를 통해 값을 조작할 수 있다.
+ *
+ * [해결]
+ * - 값을 업데이트 할 때 마다, 새로운 인스턴스를 생성해서 리턴한다.
+ * 🤔 새로운 인스턴스 -> 메모리 최적화 문제가 걱정 된다면, 계속 말하는 아직은 하지마라를 기억해라.
+ * - 최적화 < 안정성
+ */
 class Person {
   #name;
   #telephoneNumber;
@@ -15,7 +28,7 @@ class Person {
   }
 
   get telephoneNumber() {
-    return this.#telephoneNumber.toString;
+    return this.#telephoneNumber;
   }
 
   get officeAreaCode() {
@@ -23,7 +36,7 @@ class Person {
   }
 
   set officeAreaCode(value) {
-    this.#telephoneNumber.areaCode = value;
+    this.#telephoneNumber = new TelephoneNumber(value, this.officeNumber);
   }
 
   get officeNumber() {
@@ -31,7 +44,7 @@ class Person {
   }
 
   set officeNumber(value) {
-    this.#telephoneNumber.number = value;
+    this.#telephoneNumber = new TelephoneNumber(this.officeAreaCode, value);
   }
 }
 
@@ -46,15 +59,9 @@ class TelephoneNumber {
   get areaCode() {
     return this.#areaCode;
   }
-  set areaCode(arg) {
-    this.#areaCode = arg;
-  }
 
   get number() {
     return this.#number;
-  }
-  set number(arg) {
-    this.#number = arg;
   }
 
   get toString() {
@@ -64,6 +71,8 @@ class TelephoneNumber {
 
 const person = new Person('엘리', '010', '12345678');
 console.log(person.name);
+person.officeNumber = '90244573';
+person.telephoneNumber.officeNumber = '123';
 console.log(person.officeAreaCode);
 console.log(person.officeNumber);
-console.log(person.telephoneNumber);
+console.log(person.telephoneNumber.toString);
