02babd60b8b66a2802a6165e266eaf9d9af23703
shinyuna <yunadev01@gmail.com>
2022-09-28 09:13:29 +0900

    Feat. 조건문 분해하기 (첫번째 리팩터링)

--- Refactoring/kata/ch10/10-1.js
+++ Refactoring/kata/ch10/10-1.js
@@ -1,7 +1,25 @@
+/**
+ * :NOTE
+ *
+ * [문제점]
+ * - 코드의 의도를 한 눈에 파악하기 어렵다.
+ *
+ * [해결]
+ * - 부정 조건문 보단 긍정 조건문이 눈의 흐름(?)에 따라 읽기 쉽다고 생각해서 긍정 조건문으로 바꿔주기
+ * - 어떤 조건을 확인하고 싶은건지 바로 파악할 수 있도록 변수로 추출 또는 질의 함수로 추출하기
+ * - 연산이 필요한 로직은 질의 함수로 추출하기
+ */
+
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+  const isSummer = date.isBefore(plan.summerStart) && date.isAfter(plan.summerEnd)
+
+  function regularCharge() {
+    return quantity * plan.summerRate;
+  }
+
+  function summerCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge
+  }
+
+  return isSummer ? summerCharge() : regularCharge();
 }
