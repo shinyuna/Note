18d19731a342ad812c0ac22a4be1a89bd2cff6b1
shinyuna <yunadev01@gmail.com>
2022-09-28 09:27:55 +0900

    Feat. 조건식 통합하기

--- Refactoring/kata/ch10/10-2.js
+++ Refactoring/kata/ch10/10-2.js
@@ -1,6 +1,9 @@
+/**
+ * [문제]
+ * - 동일한 결과 값이 필요한 조건식을 따로 따로 분리하는 것은 가독성을 떨어트린다. -> 하나의 조건 식으로 통일
+ */
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
-  return 1;
+  const isNotDisability = employee.seniority < 2 || employee.monthsDisabled > 12 || employee.isPartTime
+
+  return isNotDisability ? 0 : 1;
 }
