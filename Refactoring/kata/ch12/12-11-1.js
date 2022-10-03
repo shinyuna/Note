eb63beffb9d87a7f0d083ce4b656508d7c4714ab
shinyuna <yunadev01@gmail.com>
2022-10-03 22:34:56 +0900

    Feat: 슈퍼클래스를 위임으로 바꾸기

--- Refactoring/kata/ch12/12-11-1.js
+++ Refactoring/kata/ch12/12-11-1.js
@@ -1,3 +1,19 @@
 class List {}
 
-class Stack extends List {}
+class Stack {
+  constructor() {
+    this.storage = new List()
+  }
+
+  push() {
+    this.storage.push()
+  }
+
+  pop() {
+    this.storage.pop()
+  }
+}
+
+/**
+ * - 원하는 동작들만 외부에 노출하고, 내부에서 필요한 클래스를 사용한다.
+ */
--- Refactoring/kata/ch12/12-11-2.js
+++ Refactoring/kata/ch12/12-11-2.js
@@ -18,14 +18,14 @@ class CatalogItem {
   }
 }
 
-class Scroll extends CatalogItem {
-  constructor(id, title, tags, dataLastCleaned) {
-    super(id, title, tags);
+class Scroll  {
+  constructor(catalogItem, dataLastCleaned) {
+    this._catalogItem = catalogItem
     this._lastCleaned = dataLastCleaned;
   }
 
   needsCleaning(targetDate) {
-    const threshold = this.hasTag('revered') ? 700 : 1500;
+    const threshold = this._catalogItem.hasTag('revered') ? 700 : 1500;
 
     return this.daysSinceLastCleaning(targetDate) > threshold;
   }
@@ -34,3 +34,7 @@ class Scroll extends CatalogItem {
     return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
   }
 }
+
+/**
+ * 상속보단 필요한 정보만 위임으로 받아와서 사용할 수 있도록 한다.
+ */
