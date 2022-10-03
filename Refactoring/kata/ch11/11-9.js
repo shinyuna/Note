77a0be86b84d80ecd22cece3180030c565626675
shinyuna <yunadev01@gmail.com>
2022-10-03 15:33:18 +0900

    Feat: 함수를 명령으로 바꾸기

--- Refactoring/kata/ch11/11-9.js
+++ Refactoring/kata/ch11/11-9.js
@@ -1,20 +1,42 @@
-export function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+/**
+ * :NOTE
+ * - 절차지향적 프로그래밍 보단, 객체지향 프로그래밍을 선호하는 앨리쌤
+ * -> 독립된 모듈을 만들 수 있다.
+ *
+ * 명령 객체/클래스/패턴
+ * => Command
+ */
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+export function score(candidate, medicalExam, scoringGuide) {
+  return new Scorer(candidate, medicalExam, scoringGuide).execute();
+}
+
+class Scorer {
+  constructor(candidate, medicalExam, scoringGuide) {
+    this.candidate = candidate
+    this.medicalExam = medicalExam
+    this.scoringGuide = scoringGuide
   }
-  let certificationGrade = 'regular';
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low';
-    result -= 5;
+
+  execute() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (this.medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+
+    let certificationGrade = 'regular';
+    if (this.scoringGuide.stateWithLowCertification(this.candidate.originState)) {
+      certificationGrade = 'low';
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-  // lots more code like this
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
 
 export class ScoringGuide {
