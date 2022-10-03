7fb030cb24d203afa55b111ba041d0b87cbdea55
shinyuna <yunadev01@gmail.com>
2022-10-03 14:16:09 +0900

    Feat: 함수 매개변수화하기

--- Refactoring/kata/ch11/11-1.js
+++ Refactoring/kata/ch11/11-1.js
@@ -1,22 +1,47 @@
+/**
+ * :NOTE
+ * Side Effect : 예상하지 못한 행동을 하는 함수
+ * 질의 함수 : 연산을 통해 값을 구하는 함수. 내부 또는 외부의 값을 변경해 부수효과를 발생해서는 안된다. (= Query)
+ */
+
+
 // 예제 1
-function totalOutstandingAndSendBill() {
-  const result = customer.invoices.reduce(
+/**
+ * - And 가 문제다. 함수는 한 가지의 일만 해야한다.
+ * - totalOutStanding 을 구하는 함수와 sendBill 하는 함수를 분리한다.
+*/
+function totalOutstanding() {
+  return customer.invoices.reduce(
     (total, each) => each.amount + total,
     0
   );
-  sendBill();
-  return result;
 }
 
+function sendBill() {
+  // send Bill logic ...
+}
+
+
 // 예제 2
+/**
+ * 악당을 찾는 함수와 알람을 보내는 로직을 분리한다.
+ * - 각 역할을 하고 있는 함수가 분리 되어 있어 필요한 로직만 가져다가 사용할 수 있어 재사용성이 높아지고 가독성 또한 좋아진다.
+ */
 export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people)
+
+  if (!miscreant) {
+    return;
+  }
+  setOffAlarms(alarm, miscreant);
+}
+
+function findMiscreant(people) {
   for (const p of people) {
     if (p === 'Don') {
-      setOffAlarms(alarm, p);
       return 'Don';
     }
     if (p === 'John') {
-      setOffAlarms(alarm, p);
       return 'John';
     }
   }
--- Refactoring/kata/ch11/11-2.js
+++ Refactoring/kata/ch11/11-2.js
@@ -1,30 +1,25 @@
-// 예제 1
-function tenPercentRaise(person) {
-  person.salary = person.salary.multiply(1.1);
-}
+/**
+ * :NOTE
+ * - 공통적인 부분은 함수, 변경되는 부분은 매개변수로 받는다.
+ * - 동일한 로직을 함수로 매번 분리하는 것도 중복이다.
+ * - 리팩터링 전, 기존 로직에 대한 이해도가 높아야 안정성 있는 코드를 작성할 수 있다.
+ */
 
-function fivePercentRaise(person) {
-  person.salary = person.salary.multiply(1.05);
+// 예제 1
+function raise(person, factor) {
+  person.salary = person.salary.multiply(1 + factor);
 }
 
 // 예제 2
 export function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    withinBand(usage, 0, 100) * 0.03 + withinBand(usage, 100, 200) * 0.05 + withinBand(usage, 200, Infinity) * 0.07;
   return usd(amount);
 }
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
-}
-
-function middleBand(usage) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function topBand(usage) {
-  return usage > 200 ? usage - 200 : 0;
+function withinBand(usage, bottom, top) {
+  return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function usd(value) {
