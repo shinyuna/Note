bef9c3a24c3301796c00b6efb99a890a6832aaae
shinyuna <yunadev01@gmail.com>
2022-10-03 20:24:41 +0900

    Feat: 슈퍼 클래스 추출하기

--- Refactoring/kata/ch12/12-8.js
+++ Refactoring/kata/ch12/12-8.js
@@ -1,11 +1,14 @@
-class Department {
-  get totalAnnualCost() {}
+class Party {
   get name() {}
+  get annualCost() {}
+}
+
+class Department extends Party{
   get headCount() {}
 }
 
-class Employee {
-  get annualCost() {}
-  get name() {}
+class Employee extends Party {
   get id() {}
 }
+
+// 공통적으로 사용되는 것이 있다면 슈퍼 클래스를 사용해볼수 없는지 고민하고 적용해보기
