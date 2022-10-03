8c7adaf02b31453f0c281ad52cbc54a12d1d884f
shinyuna <yunadev01@gmail.com>
2022-10-03 19:34:34 +0900

    Feat: 필드 내리기
    - 처음부터 완벽한 코드를 작성할 순 없다. 서비스에 대한 이해도가 높아질 때 마다 코드를 리팩터링 해나가면서 좋은 코드를 작성하는게 중요하다.

--- Refactoring/kata/ch12/12-5.js
+++ Refactoring/kata/ch12/12-5.js
@@ -1,6 +1,12 @@
 class Employee {
-  #quota;
 }
 
 class Engineer extends Employee {}
-class Salesperson extends Employee {}
+class Salesperson extends Employee {
+  #quota;
+}
+
+/**
+ * :NOTE
+ * 처음부터 완벽한 코드를 작성할 순 없다. 서비스에 대한 이해도가 높아질 때 마다 코드를 리팩터링 해나가면서 좋은 코드를 작성하는게 중요하다.
+ */
