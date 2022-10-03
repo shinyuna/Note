2ecf15ceab22b9f323cff59228942d16c4ee58a1
shinyuna <yunadev01@gmail.com>
2022-10-03 16:00:49 +0900

    Feat: 예외를 사전확인으로 바꾸기

--- Refactoring/kata/ch11/11-13.js
+++ Refactoring/kata/ch11/11-13.js
@@ -1,14 +1,18 @@
 const values = [];
 function getValueForPeriod(periodNumber) {
-  const value = values[periodNumber];
-  if (!value) {
-    throw new Error('value is undefined');
-  }
-  return value;
+  // if (periodNumber < 0 || periodNumber >= values.length) {
+  //   return 0;
+  // }
+  return values[periodNumber] ?? 0;
 }
 
-try {
-  getValueForPeriod(-10);
-} catch (error) {
-  console.log('에러 발생!');
-}
+getValueForPeriod(-10);
+
+/**
+ * :NOTE
+ * - 예외는 예상하지 못한 상황에서 사용하는 것이다. (ex. 네트워크 에러)
+ * -> 예외 남용 NOPE!!!
+ * -> 불필요한 예외 처리로 오히려 사용하는 쪽의 코드가 더러워질 수 있다.
+ *
+ * - Expected = 예상할 수 있는 에러는 상황에 맞게 각각의 코드를 작성해서 처리한다.
+ */
