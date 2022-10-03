7cffc07cc03455bdcffe1429e0986d8438563433
shinyuna <yunadev01@gmail.com>
2022-10-03 14:56:57 +0900

    Feat: 객체 통채로 넘기기

--- Refactoring/kata/ch11/11-4.js
+++ Refactoring/kata/ch11/11-4.js
@@ -1,8 +1,13 @@
+/**
+ * :NOTE
+ * - ⚠️
+ * - 객체와 함수의 의존성 관계를 주의 깊게 생각해보고 전달하도록 한다.
+ * - 객체를 통채로 넘기면서 불필요한 데이터까지 전달하게 되는건 아닌지? 모듈들간 tight coupling 이 발생된 것은 아닌지?
+ */
 export function temperatureAlerts(room, plan) {
   const alerts = [];
-  const low = room.daysTempRange.low;
-  const high = room.daysTempRange.high;
-  if (!plan.withinRange(low, high)) {
+
+  if (!plan.withinRange(room.daysTempRange)) {
     alerts.push('room temperature went outside range');
   }
 
@@ -14,9 +19,9 @@ export class HeatingPlan {
     this._temperatureRange = temperatureRange;
   }
 
-  withinRange(bottom, top) {
+  withinRange(range) {
     return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+      range.bottom >= this._temperatureRange.low && range.top <= this._temperatureRange.high
     );
   }
 }
