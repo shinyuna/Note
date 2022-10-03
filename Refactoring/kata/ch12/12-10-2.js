54847c7939069c0cdfe47627f04b4d3a5243d3af
shinyuna <yunadev01@gmail.com>
2022-10-04 00:14:19 +0900

    Feat: 서브 클래스를 위임으로 바꾸기
    - 일단, delegate 클래스를 전부 생성해서 해당 위임 클래스를 선택하도록 리팩터링한다.
    - 이떄의 문제점, 노르웨이 파랑 앵무에만 존재하는 plumage 로 그 외의 delegate 클래스에선 오류가 발생하게 된다.

--- Refactoring/kata/ch12/12-10-2.js
+++ Refactoring/kata/ch12/12-10-2.js
@@ -1,7 +1,12 @@
+function createBird(bird) {
+  return new Bird(bird);
+}
+
 class Bird {
   constructor(data) {
     this._name = data.name;
     this._plumage = data.plumage;
+    this._specialDelegate = this.selectSpecialDelegate(data)
   }
 
   get name() {
@@ -9,37 +14,44 @@ class Bird {
   }
 
   get plumage() {
+    if (this._specialDelegate) {
+      return this._specialDelegate.plumage
+    }
     return this._plumage || '보통이다';
   }
 
   get airSpeedVelocity() {
-    return null;
+    return this._specialDelegate ? this._specialDelegate.airSpeedVelocity : null;
   }
 
-  static createBird(bird) {
-    switch (bird.type) {
+  selectSpecialDelegate(data) {
+    switch (data.type) {
       case '유럽 제비':
-        return new EuropeanSwallow(bird);
+        return new EuropeanSwallowDelegate()
       case '아프리카 제비':
-        return new AfricanSwallow(bird);
+        return new AfricanSwallowDelegate(data);
       case '노르웨이 파랑 앵무':
-        return new NorwegianBlueParrot(bird);
+        return new NorwegianBlueParrotDelegate(data, this);
       default:
-        return new Bird(bird);
+        return null;
     }
   }
 }
 
-class EuropeanSwallow extends Bird {
+class EuropeanSwallowDelegate {
   get airSpeedVelocity() {
     return 35;
   }
 }
 
-class AfricanSwallow extends Bird {
-  constructor(data) {
-    super(data);
+class EuropeanSwallow extends Bird {
+  get airSpeedVelocity() {
+    return this._specialDelegate.airSpeedVelocity;
+  }
+}
 
+class AfricanSwallowDelegate {
+  constructor(data) {
     this._numberOfCoconuts = data.numberOfCoconuts;
   }
 
@@ -48,23 +60,30 @@ class AfricanSwallow extends Bird {
   }
 }
 
-class NorwegianBlueParrot extends Bird {
-  constructor(data) {
-    super(data);
+class AfricanSwallow extends Bird {
+  get airSpeedVelocity() {
+    return this._specialDelegate.airSpeedVelocity;
+  }
+}
 
+class NorwegianBlueParrotDelegate {
+  constructor(data, bird) {
+    this._bird = bird
     this._voltage = data.voltage;
     this._isNailed = data.isNailed;
   }
 
   get plumage() {
-    if (this._voltage > 100) {
-      return '그을렸다';
-    } else {
-      return this._plumage || '예쁘다';
-    }
+    return this._voltage > 100 ? '그을렸다' : (this._bird._plumage || '예쁘다')
   }
 
   get airSpeedVelocity() {
     return this._isNailed ? 0 : 10 + this._voltage / 10;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  get airSpeedVelocity() {
+    return this._specialDelegate.airSpeedVelocity;
   }
 }
