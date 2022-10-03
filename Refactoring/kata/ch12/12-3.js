17141c2a9e1a581f3bb5019191fcc2209b3dac54
shinyuna <yunadev01@gmail.com>
2022-10-03 19:31:09 +0900

    Feat: 생성자 본문 올리기
    - 메서드/필드 올리기와 같은 내용
    - 하위 클래스에서 공통적으로 사용되는 내용은 상위 클래스에서 처리하도록 설계하는게 코드 중복을 줄이는데도 효과적이다.

--- Refactoring/kata/ch12/12-3.js
+++ Refactoring/kata/ch12/12-3.js
@@ -1,23 +1,25 @@
-class Party {}
+class Party {
+  #name;
+  constructor(name) {
+    this.#name = name;
+  }
+}
+
 
 class Employee extends Party {
-  #name;
   #id;
   #monthlyCost;
   constructor(name, id, monthlyCost) {
-    super();
-    this.#name = name;
+    super(name);
     this.#id = id;
     this.#monthlyCost = monthlyCost;
   }
 }
 
 class Department extends Party {
-  #name;
   #staff;
   constructor(name, staff) {
-    super();
-    this.#name = name;
+    super(name);
     this.#staff = staff;
   }
 }
