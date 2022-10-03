46b2ee056860a39664f1834d15e635096ee93e3c
shinyuna <yunadev01@gmail.com>
2022-10-03 15:48:56 +0900

    Feat: 수정된 값 반환하기

--- Refactoring/kata/ch11/11-11.js
+++ Refactoring/kata/ch11/11-11.js
@@ -1,13 +1,20 @@
+/**
+ * :NOTE
+ * - 내부에서 전달 받은 값이나 외부 값을 변경하는 것은 사이드 이펙트를 발생한다.
+ */
+
 export function ascentVelocity(points, totalMinutes) {
-  function calculateAscent() {
+
+  function calculateAscent(points) {
+    let result = 0;
     for (let i = 1; i < points.length; i++) {
       const verticalChange = points[i].elevation - points[i - 1].elevation;
-      totalAscent += verticalChange > 0 ? verticalChange : 0;
+      result += verticalChange > 0 ? verticalChange : 0;
     }
+    return result;
   }
 
-  let totalAscent = 0;
-  calculateAscent([{ elevation: 10 }, { elevation: 20 }]);
+  const totalAscent = calculateAscent([{ elevation: 10 }, { elevation: 20 }]);
 
   return totalAscent / totalMinutes;
 }
