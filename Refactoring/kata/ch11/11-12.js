086ef65225390535f3db7c6b00aaa20abf481026
shinyuna <yunadev01@gmail.com>
2022-10-03 15:54:48 +0900

    Feat: 오류 코드를 예외로 바꾸기(

--- Refactoring/kata/ch11/11-12.js
+++ Refactoring/kata/ch11/11-12.js
@@ -1,4 +1,25 @@
+/**
+ * :NOTE
+ * - 에러 코드는 프로그래밍에서 제공해주는 에러 클래스를 반환한다.
+ * - 에러 코드는 일반적인 값보단, 에러의 의미를 정확하게 알 수 있는 객체를 사용한다.
+ */
+
 function localShippingRules(data) {
   if (data) return new ShippingRules(data);
-  else return -23;
+  else throw new OrderProcessingError(-23)
+}
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super();
+    this.errorCode = errorCode
+  }
+}
+
+try {
+  const result = localShippingRules(data)
+} catch (e) {
+  if (e instanceof OrderProcessingError) {
+    console.log(e)
+  }
 }
