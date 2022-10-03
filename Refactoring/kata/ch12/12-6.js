08c4b08ded31883abc17487a1cfac99b8746d40f
shinyuna <yunadev01@gmail.com>
2022-10-03 20:10:20 +0900

    Feat: 타입 코드를 서브클래스로 바꾸기

--- Refactoring/kata/ch12/12-6.js
+++ Refactoring/kata/ch12/12-6.js
@@ -1,26 +1,54 @@
 class Employee {
   #name;
-  #type;
-  constructor(name, type) {
-    this.validateType(type);
+  constructor(name) {
     this.#name = name;
-    this.#type = type;
-  }
-
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
   }
 
   get type() {
-    return this.#type;
+    return 'employee'
   }
 
   toString() {
     return `${this.#name} (${this.type})`;
   }
+
+  static createEmployee(name, type) {
+    switch (type) {
+      case 'engineer':
+        return new Engineer(name)
+      case 'manager':
+        return new Manager(name)
+      case 'salesperson':
+        return new Salesperson(name)
+      default:
+        return throw new Error(`${type}라는 직원 유형은 없습니다.`);
+    }
+  }
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+class Engineer extends Employee {
+  get type() {
+    return 'engineer'
+  }
+}
+
+class Manager extends Employee {
+  get type() {
+    return 'manager'
+  }
+}
+
+class Salesperson extends Employee {
+  get type() {
+    return 'manager'
+  }
+}
+
+const ellie = new Engineer('엘리');
+const bob = new Manager('밥');
+
+/**
+ * [문제]
+ * - employee type 속성이 있지만, 어떤 type 을 정의 할 수 있는지 알 수가 없다.
+ * - 직접 정의 할 수 없는 상황(ex. 서버의 JSON 데이터를 통해 인스턴스를 만들 때)엔 팩토리 패턴을 통해 인스턴스를 만들도록 한다.
+ */
