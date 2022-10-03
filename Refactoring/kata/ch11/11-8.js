0933a864bbf52aec3fe7c54ada0576d28bbec6c8
shinyuna <yunadev01@gmail.com>
2022-10-03 15:23:41 +0900

    Feat: 생성자를 팩터리 함수로 바꾸기

--- Refactoring/kata/ch11/11-8.js
+++ Refactoring/kata/ch11/11-8.js
@@ -1,3 +1,10 @@
+/**
+ * :NOTE
+ * 팩토리 함수 : 인스턴스를 만드는 로직 자체를 캡슐화 해서 외부에서 간단하게 인스턴스를 생성하도록 할 때 사용한다.
+ * - javascript 외의 다른 언어에서는 constructor 함수를 private 로 설정할 수 있어 외부에서 생성자 함수에 접근하지 못하도록 할 수 있다.
+ * - 외부에서 생성자 함수에 직접 접근해서 인스턴스를 생성하는 것이 아니라, 미리 정의한 팩토리 함수를 통해 인스턴스를 생성한다.
+ */
+
 export class Employee {
   constructor(name, typeCode) {
     this._name = name;
@@ -14,4 +21,11 @@ export class Employee {
   static get legalTypeCodes() {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
+
+  static createEngineer(name) {
+    return new Employee(name, 'E')
+  }
 }
+
+const engineer1 = new Employee('융디', 'E') // X
+const engineer2 = Employee.createEngineer('융디') // O
