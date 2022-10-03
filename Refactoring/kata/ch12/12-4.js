c5efd448a27d81fd37af0aed95f38f110841e820
shinyuna <yunadev01@gmail.com>
2022-10-03 19:32:57 +0900

    Feat: 메서드 내리기

--- Refactoring/kata/ch12/12-4.js
+++ Refactoring/kata/ch12/12-4.js
@@ -1,6 +1,13 @@
 class Employee {
-  get quota() {}
 }
 
 class Engineer extends Employee {}
-class Salesperson extends Employee {}
+class Salesperson extends Employee {
+  get quota() {}
+}
+
+/**
+ * :NOTE
+ * 특정한 서브 클래스에서만 사용된다면, 서브 클래스에서만 정의하는게 좋다.
+ * 모든 서브 클래스에서 사용되는 것이 명확해질 때 상위 클래스에 올린다.
+ */
