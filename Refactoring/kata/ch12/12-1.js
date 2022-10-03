108293f93dc89be9150cb1855de3b9576fca7923
shinyuna <yunadev01@gmail.com>
2022-10-03 19:24:43 +0900

    Feat: 메서드 올리기

--- Refactoring/kata/ch12/12-1.js
+++ Refactoring/kata/ch12/12-1.js
@@ -1,24 +1,28 @@
+/**
+ * :NOTE
+ * - 하위 클래스에서 사용되는 공통된 메서드를 상위 클래스에서 관리한다.
+ * - 하위 클래스에서 같은 클래스를 중구난방 다른 이름으로 사용하고 있는 것 보단, 상위 클래스에서 정의한 메서드를 동일하게 사용하고 있는 것이 재사용성/가독성 측면에서 더 좋다.
+ */
+
 // 예시 1
-class Employee {}
+class Employee {
+  get name() {}
+}
 
 class Salesperson extends Employee {
-  get name() {}
 }
 
 class Engineer extends Employee {
-  get name() {}
 }
 
 // 예시 2
-class Party {}
-
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
-  }
-}
-class Employee extends Party {
+class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
+}
+
+class Department extends Party {
+}
+class Employee extends Party {
 }
