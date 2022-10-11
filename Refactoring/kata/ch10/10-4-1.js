72c4a64f8dc2d05346bb53553ba9a960f581023d
신유나 <yunadev01@gmail.com>
2022-10-12 00:27:22 +0900

    Feat: 조건부 로직을 다형성으로 바꾸기 1

--- Refactoring/kata/ch10/10-4-1.js
+++ Refactoring/kata/ch10/10-4-1.js
@@ -1,60 +1,45 @@
 /**
  *
  * :NOTE
- * - 리팩터링 추상화 레벨 : 객체 지향 관련 내용
- * - 프로젝트 전체 설계를 어떻게 할지? -> OOP 학습
+ * - 리팩터링 추상화 레벨 : 객체 지향 프로그래밍에 관련 내용
+ * - 프로젝트 전체 설계를 어떻게 개선할 수 있을지? => OOP 학습
  * - 객체들간의 공통된 특징을 찾아서 클래스 할 수 있다면 해라
  *
- * 유지보수성
+ * #다형성
+ * - 추상화를 통해 공통된 특징을 추출해서 Super 클래스로 만들고, 공통되지 않는 내용을 추출해 각자 타입(클래스에)에 맞게 관리한다.
+ * - 이때, 중요 포인트는 Super 클래스의 특징이 모든 클래스의 특징과 일치해야 한다는 점.
+ * - 내부 로직, 반환 값은 달라도 시그니처가 같아야 한다(?)
+ * -> 유지보수하기 좋아진다.
  *
  * [문제]
  * - 조건이 추가 될 때 마다 추가 되는 switch case
  */
 
 export function plumages(birds) {
-  let map = birds.map((b) => [b.name, plumage(b)]);
+  let map = birds.map(b => [b.name, b.plumage]);
   let map1 = new Map(map);
   return map1;
 }
+
 export function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
-}
-export function plumage(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 'average';
-    case 'AfricanSwallow':
-      return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
-    case 'NorwegianBlueParrot':
-      return bird.voltage > 100 ? 'scorched' : 'beautiful';
-    default:
-      return 'unknown';
-  }
-}
-export function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 35;
-    case 'AfricanSwallow':
-      return 40 - 2 * bird.numberOfCoconuts;
-    case 'NorwegianBlueParrot':
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
-  }
+  return new Map(birds.map(b => [b.name, b.airSpeedVelocity]));
 }
 
 class Bird {
-  #name
+  #name;
   constructor(name) {
-    this.#name = name
+    this.#name = name;
   }
 
-  plumage() {
-    return 'unknown'
+  get name() {
+    return this.#name;
   }
 
-  airSpeedVelocity() {
+  get plumage() {
+    return 'unknown';
+  }
+
+  get airSpeedVelocity() {
     return null;
   }
 }
@@ -64,6 +49,37 @@ class EuropeanSwallow extends Bird {
     super('EuropeanSwallow');
   }
 
+  get plumage() {
+    return 'average';
+  }
 
+  get airSpeedVelocity() {
+    return 35;
+  }
 }
+class AfricanSwallow extends Bird {
+  constructor() {
+    super('AfricanSwallow');
+  }
 
+  get plumage() {
+    return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * bird.numberOfCoconuts;
+  }
+}
+class NorwegianBlueParrot extends Bird {
+  constructor() {
+    super('NorwegianBlueParrot');
+  }
+
+  get plumage() {
+    return bird.voltage > 100 ? 'scorched' : 'beautiful';
+  }
+
+  get airSpeedVelocity() {
+    return bird.isNailed ? 0 : 10 + bird.voltage / 10;
+  }
+}
