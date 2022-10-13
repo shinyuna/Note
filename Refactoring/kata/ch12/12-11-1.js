29ae5d591543d70866d145e0979ef8df5834a181
shinyuna <yunadev01@gmail.com>
2022-10-13 23:20:33 +0900

    Docs: 내용 보충

--- Refactoring/kata/ch12/12-11-1.js
+++ Refactoring/kata/ch12/12-11-1.js
@@ -2,7 +2,7 @@ class List {}
 
 class Stack {
   constructor() {
-    this.storage = new List()
+    this.storage = new List() // 캡슐화를 위함
   }
 
   push() {
@@ -16,4 +16,8 @@ class Stack {
 
 /**
  * - 원하는 동작들만 외부에 노출하고, 내부에서 필요한 클래스를 사용한다.
+ *
+ * TIP
+ * - 위 예제에서는 캡슐화를 위해 생성자에서 인스턴스를 생성하지만, DI를 사용해서 의존성을 낮출 수 있다.
+ * - "모든 것은 선택이다. 상황에 맞게 잘 선택하면 된다."
  */
