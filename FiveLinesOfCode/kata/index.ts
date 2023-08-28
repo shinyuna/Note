731a21d5c99b48ba6ca3ebc31cb682cc61bd6c67
신유나 <yunadev01@gmail.com>
2023-08-29 01:03:15 +0900

    refactor: 주석을 기점으로 '메서드 추출'
    - 주석을 메서드명으로 대체

--- FiveLinesOfCode/kata/index.ts
+++ FiveLinesOfCode/kata/index.ts
@@ -117,7 +117,17 @@ function draw() {
 
   g.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw map
+  drawMap(g);
+
+  drawPlayer(g);
+}
+
+function drawPlayer(g: CanvasRenderingContext2D) {
+  g.fillStyle = '#ff0000';
+  g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+}
+
+function drawMap(g: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
       if (map[y][x] === Tile.FLUX) g.fillStyle = '#ccffcc';
@@ -131,10 +141,6 @@ function draw() {
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
   }
-
-  // Draw player
-  g.fillStyle = '#ff0000';
-  g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
 function gameLoop() {
