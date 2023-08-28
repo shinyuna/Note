ef0460a8faa1f2eacbfa054fe80d8d47368bf3e8
신유나 <yunadev01@gmail.com>
2023-08-29 02:12:33 +0900

    refactor: if 문은 함수의 시작에만 위치
    - if 문 또한 하나의 작업이며, 하나의 함수에서 처리해야 한다.

--- FiveLinesOfCode/kata/index.ts
+++ FiveLinesOfCode/kata/index.ts
@@ -86,29 +86,44 @@ function moveVertical(dy: number) {
 }
 
 function update() {
-  while (inputs.length > 0) {
-    let current = inputs.pop();
-    if (current === Input.LEFT) moveHorizontal(-1);
-    else if (current === Input.RIGHT) moveHorizontal(1);
-    else if (current === Input.UP) moveVertical(-1);
-    else if (current === Input.DOWN) moveVertical(1);
-  }
+  handleInputs();
+  updateMap();
+}
 
+function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
-      if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) && map[y + 1][x] === Tile.AIR) {
-        map[y + 1][x] = Tile.FALLING_STONE;
-        map[y][x] = Tile.AIR;
-      } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) && map[y + 1][x] === Tile.AIR) {
-        map[y + 1][x] = Tile.FALLING_BOX;
-        map[y][x] = Tile.AIR;
-      } else if (map[y][x] === Tile.FALLING_STONE) {
-        map[y][x] = Tile.STONE;
-      } else if (map[y][x] === Tile.FALLING_BOX) {
-        map[y][x] = Tile.BOX;
-      }
+      updateTile(y, x);
     }
   }
+}
+
+function updateTile(y: number, x: number) {
+  if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) && map[y + 1][x] === Tile.AIR) {
+    map[y + 1][x] = Tile.FALLING_STONE;
+    map[y][x] = Tile.AIR;
+  } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) && map[y + 1][x] === Tile.AIR) {
+    map[y + 1][x] = Tile.FALLING_BOX;
+    map[y][x] = Tile.AIR;
+  } else if (map[y][x] === Tile.FALLING_STONE) {
+    map[y][x] = Tile.STONE;
+  } else if (map[y][x] === Tile.FALLING_BOX) {
+    map[y][x] = Tile.BOX;
+  }
+}
+
+function handleInputs() {
+  while (inputs.length > 0) {
+    let current = inputs.pop();
+    handleInput(current);
+  }
+}
+
+function handleInput(input: Input) {
+  if (input === Input.LEFT) moveHorizontal(-1);
+  else if (input === Input.RIGHT) moveHorizontal(1);
+  else if (input === Input.UP) moveVertical(-1);
+  else if (input === Input.DOWN) moveVertical(1);
 }
 
 function draw() {
