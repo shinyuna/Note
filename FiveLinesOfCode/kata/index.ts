f2c1ffefe628ad24d3f1dd73315479f9bbe51c72
신유나 <yunadev01@gmail.com>
2023-08-31 01:46:10 +0900

    refactor: 클래스로 타입코드 대체하기
    - 값과 관련된 메서드 그룹화 가능

--- FiveLinesOfCode/kata/index.ts
+++ FiveLinesOfCode/kata/index.ts
@@ -17,7 +17,7 @@ enum Tile {
   LOCK2,
 }
 
-enum Input {
+enum RawInput {
   UP,
   DOWN,
   LEFT,
@@ -35,7 +35,7 @@ let map: Tile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
 
-let inputs: Input[] = [];
+let inputs: RawInput[] = [];
 
 function remove(tile: Tile) {
   for (let y = 0; y < map.length; y++) {
@@ -119,11 +119,88 @@ function handleInputs() {
   }
 }
 
-function handleInput(input: Input) {
-  if (input === Input.LEFT) moveHorizontal(-1);
-  else if (input === Input.RIGHT) moveHorizontal(1);
-  else if (input === Input.UP) moveVertical(-1);
-  else if (input === Input.DOWN) moveVertical(1);
+interface Input2 {
+  isRight(): boolean;
+  isLeft(): boolean;
+  isUp(): boolean;
+  isDown(): boolean;
+}
+
+class Right implements Input2 {
+  isRight() {
+    return true;
+  }
+
+  isLeft() {
+    return false;
+  }
+
+  isUp() {
+    return false;
+  }
+
+  isDown() {
+    return false;
+  }
+}
+class LEFT implements Input2 {
+  isRight() {
+    return false;
+  }
+
+  isLeft() {
+    return true;
+  }
+
+  isUp() {
+    return false;
+  }
+
+  isDown() {
+    return false;
+  }
+}
+class UP implements Input2 {
+  isRight() {
+    return false;
+  }
+
+  isLeft() {
+    return false;
+  }
+
+  isUp() {
+    return true;
+  }
+
+  isDown() {
+    return false;
+  }
+}
+
+class Down implements Input2 {
+  isRight() {
+    return false;
+  }
+
+  isLeft() {
+    return false;
+  }
+
+  isUp() {
+    return false;
+  }
+
+  isDown() {
+    return true;
+  }
+}
+
+function handleInput(input: Input2) {
+  if (input.isLeft) moveHorizontal(-1);
+  else if (input.isRight) moveHorizontal(1);
+  else if (input.isUp) moveVertical(-1);
+  else if (input.isDown) moveVertical(1);
 }
 
 function draw() {
@@ -181,8 +258,8 @@ const UP_KEY = 'ArrowUp';
 const RIGHT_KEY = 'ArrowRight';
 const DOWN_KEY = 'ArrowDown';
 window.addEventListener('keydown', e => {
-  if (e.key === LEFT_KEY || e.key === 'a') inputs.push(Input.LEFT);
-  else if (e.key === UP_KEY || e.key === 'w') inputs.push(Input.UP);
-  else if (e.key === RIGHT_KEY || e.key === 'd') inputs.push(Input.RIGHT);
-  else if (e.key === DOWN_KEY || e.key === 's') inputs.push(Input.DOWN);
+  if (e.key === LEFT_KEY || e.key === 'a') inputs.push(RawInput.LEFT);
+  else if (e.key === UP_KEY || e.key === 'w') inputs.push(RawInput.UP);
+  else if (e.key === RIGHT_KEY || e.key === 'd') inputs.push(RawInput.RIGHT);
+  else if (e.key === DOWN_KEY || e.key === 's') inputs.push(RawInput.DOWN);
 });
