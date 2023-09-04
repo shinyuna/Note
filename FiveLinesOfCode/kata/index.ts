ee17550099b814e1ad46efe7a6ff002f91a9dd2a
신유나 <yunadev01@gmail.com>
2023-09-05 02:26:19 +0900

    refactor: 타입을 클래스로 대체 & 메서드 전문화
    - Tile 열거형을 Tile2 abstract 클래스로 대체
    - remove 메서드를 각 상황에 맞는 전문화된 메서드로 리팩터링

--- FiveLinesOfCode/kata/index.ts
+++ FiveLinesOfCode/kata/index.ts
@@ -2,7 +2,7 @@ const TILE_SIZE = 30;
 const FPS = 30;
 const SLEEP = 1000 / FPS;
 
-enum Tile {
+enum RawTile {
   AIR,
   FLUX,
   UNBREAKABLE,
@@ -16,6 +16,57 @@ enum Tile {
   KEY2,
   LOCK2,
 }
+abstract class Tile {
+  isAir() { return false; }
+  isFlux() { return false; }
+  isUnbreakable() { return false; }
+  isPlayer() { return false; }
+  isStone() { return false; }
+  isBox() { return false; }
+  isKey1() { return false; }
+  isKey2() { return false; }
+  isFallingStone() { return false; }
+  isFallingBox() { return false; }
+  isLock1() { return false; }
+  isLock2() { return false; }
+}
+class Air extends Tile {
+  isAir() { return true; }
+}
+class Flux extends Tile {
+  isFlux() { return true; }
+}
+class Unbreakable extends Tile {
+  isUnbreakable() { return true; }
+}
+class Player extends Tile {
+  isPlayer() { return true; }
+}
+class Stone extends Tile {
+  isStone() { return true; }
+}
+class Box extends Tile {
+  isBox() { return true; }
+}
+class Key1 extends Tile {
+  isKey1() { return true; }
+}
+class Key2 extends Tile {
+  isKey2() { return true; }
+}
+class FallingStone extends Tile {
+  isFallingStone() { return true; }
+}
+class FallingBox extends Tile {
+  isFallingBox() { return true; }
+}
+class Lock1 extends Tile {
+  isLock1() { return true; }
+}
+class Lock2 extends Tile {
+  isLock2() { return true; }
+}
+
 
 enum RawInput {
   Up,
@@ -26,7 +77,7 @@ enum RawInput {
 
 let playerx = 1;
 let playery = 1;
-let map: Tile[][] = [
+let rawMap: RawTile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
   [2, 3, 0, 1, 1, 2, 0, 2],
   [2, 4, 2, 6, 1, 2, 0, 2],
@@ -34,63 +85,111 @@ let map: Tile[][] = [
   [2, 4, 1, 1, 1, 9, 0, 2],
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
+let map: Tile[][];
+function transformTile(title: RawTile) {
+  switch (title) {
+    case RawTile.AIR:
+      return new Air();
+    case RawTile.FLUX:
+      return new Flux();
+    case RawTile.UNBREAKABLE:
+      return new Unbreakable();
+    case RawTile.PLAYER:
+      return new Player();
+    case RawTile.STONE:
+      return new Stone();
+    case RawTile.FALLING_STONE:
+      return new FallingStone();
+    case RawTile.BOX:
+      return new Box();
+    case RawTile.FALLING_BOX:
+      return new FallingBox();
+    case RawTile.KEY1:
+      return new Key1();
+    case RawTile.KEY2:
+      return new Key2();
+    case RawTile.LOCK1:
+      return new Lock1();
+    case RawTile.LOCK2:
+      return new Lock2();
+  }
+}
+
+function trnasformMap() {
+  map = new Array(rawMap.length);
+  for (let y = 0; y < rawMap.length; y++) {
+    map[y] = new Array(rawMap[y].length);
+    for (let x = 0; x < rawMap[y].length; x++) {
+      map[y][x] = transformTile(rawMap[y][x]);
+    }
+  }
+}
 
 let inputs: Input[] = [];
 
-function remove(tile: Tile) {
+function removeLock1() {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === tile) {
-        map[y][x] = Tile.AIR;
+      if (map[y][x].isLock1()) {
+        map[y][x] = new Air();
+      }
+    }
+  }
+}
+function removeLock2() {
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x].isLock2()) {
+        map[y][x] = new Air();
       }
     }
   }
 }
 
 function moveToTile(newx: number, newy: number) {
-  map[playery][playerx] = Tile.AIR;
-  map[newy][newx] = Tile.PLAYER;
+  map[playery][playerx] = new Air();
+  map[newy][newx] = new Player();
   playerx = newx;
   playery = newy;
 }
 
 function moveHorizontal(dx: number) {
-  if (map[playery][playerx + dx] === Tile.FLUX || map[playery][playerx + dx] === Tile.AIR) {
+  if (map[playery][playerx + dx].isFlux() || map[playery][playerx + dx].isAir()) {
     moveToTile(playerx + dx, playery);
   } else if (
-    (map[playery][playerx + dx] === Tile.STONE || map[playery][playerx + dx] === Tile.BOX) &&
-    map[playery][playerx + dx + dx] === Tile.AIR &&
-    map[playery + 1][playerx + dx] !== Tile.AIR
+    (map[playery][playerx + dx].isStone() || map[playery][playerx + dx].isBox()) &&
+    map[playery][playerx + dx + dx].isAir() &&
+    !map[playery + 1][playerx + dx].isAir()
   ) {
     map[playery][playerx + dx + dx] = map[playery][playerx + dx];
     moveToTile(playerx + dx, playery);
-  } else if (map[playery][playerx + dx] === Tile.KEY1) {
-    remove(Tile.LOCK1);
+  } else if (map[playery][playerx + dx].isKey1()) {
+    removeLock1();
     moveToTile(playerx + dx, playery);
-  } else if (map[playery][playerx + dx] === Tile.KEY2) {
-    remove(Tile.LOCK2);
+  } else if (map[playery][playerx + dx].isKey2()) {
+    removeLock2();
     moveToTile(playerx + dx, playery);
   }
 }
 
 function moveVertical(dy: number) {
-  if (map[playery + dy][playerx] === Tile.FLUX || map[playery + dy][playerx] === Tile.AIR) {
+  if (map[playery + dy][playerx].isFlux() || map[playery + dy][playerx] === new Air()) {
     moveToTile(playerx, playery + dy);
-  } else if (map[playery + dy][playerx] === Tile.KEY1) {
-    remove(Tile.LOCK1);
+  } else if (map[playery + dy][playerx].isKey1()) {
+    removeLock1();
     moveToTile(playerx, playery + dy);
-  } else if (map[playery + dy][playerx] === Tile.KEY2) {
-    remove(Tile.LOCK2);
+  } else if (map[playery + dy][playerx].isKey2()) {
+    removeLock2();
     moveToTile(playerx, playery + dy);
   }
 }
 
 function update() {
   handleInputs();
-  updateMap();
+  updatemap();
 }
 
-function updateMap() {
+function updatemap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
       updateTile(y, x);
@@ -99,16 +198,16 @@ function updateMap() {
 }
 
 function updateTile(y: number, x: number) {
-  if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) && map[y + 1][x] === Tile.AIR) {
-    map[y + 1][x] = Tile.FALLING_STONE;
-    map[y][x] = Tile.AIR;
-  } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) && map[y + 1][x] === Tile.AIR) {
-    map[y + 1][x] = Tile.FALLING_BOX;
-    map[y][x] = Tile.AIR;
-  } else if (map[y][x] === Tile.FALLING_STONE) {
-    map[y][x] = Tile.STONE;
-  } else if (map[y][x] === Tile.FALLING_BOX) {
-    map[y][x] = Tile.BOX;
+  if ((map[y][x].isStone()  || map[y][x].isFallingStone()) && map[y + 1][x].isAir()) {
+    map[y + 1][x] = new FallingStone();
+    map[y][x] = new Air();
+  } else if ((map[y][x].isBox() || map[y][x].isFallingBox()) && map[y + 1][x].isAir()) {
+    map[y + 1][x] = new FallingBox();
+    map[y][x] = new Air();
+  } else if (map[y][x].isFallingStone()) {
+    map[y][x] = new Stone();
+  } else if (map[y][x].isFallingBox()) {
+    map[y][x] = new Box();
   }
 }
 
@@ -238,19 +337,19 @@ function drawMap(g: CanvasRenderingContext2D) {
     for (let x = 0; x < map[y].length; x++) {
       colorOfTile(g, y, x);
 
-      if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
+      if (!map[y][x].isAir() && !map[y][x].isPlayer())
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
   }
 }
 
 function colorOfTile(g: CanvasRenderingContext2D, y: number, x: number) {
-  if (map[y][x] === Tile.FLUX) g.fillStyle = '#ccffcc';
-  else if (map[y][x] === Tile.UNBREAKABLE) g.fillStyle = '#999999';
-  else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) g.fillStyle = '#0000cc';
-  else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) g.fillStyle = '#8b4513';
-  else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1) g.fillStyle = '#ffcc00';
-  else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2) g.fillStyle = '#00ccff';
+  if (map[y][x].isFlux()) g.fillStyle = '#ccffcc';
+  else if (map[y][x].isUnbreakable()) g.fillStyle = '#999999';
+  else if (map[y][x].isStone() || map[y][x].isFallingStone()) g.fillStyle = '#0000cc';
+  else if (map[y][x].isBox() || map[y][x].isFallingBox()) g.fillStyle = '#8b4513';
+  else if (map[y][x].isKey1() || map[y][x].isLock1()) g.fillStyle = '#ffcc00';
+  else if (map[y][x].isKey2() || map[y][x].isLock2()) g.fillStyle = '#00ccff';
 }
 
 function gameLoop() {
@@ -264,6 +363,7 @@ function gameLoop() {
 }
 
 window.onload = () => {
+  trnasformMap();
   gameLoop();
 };
 
